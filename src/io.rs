6554ffd8689aed9d8cac8244e1d3e4898520c77d
Lion Kortlepel <development@kortlepel.com>
2026-01-21 21:08:05 +0100

    apply clippy suggestions

--- src/io.rs
+++ src/io.rs
@@ -9,15 +9,15 @@ pub struct PasteStore {
 impl PasteStore {
     pub fn new(db_path: &str) -> Result<Self, Box<dyn std::error::Error>> {
         let db = sqlite::Connection::open_thread_safe(db_path)?;
-        db.execute(r#"CREATE TABLE IF NOT EXISTS pastes (id TEXT, data BLOB);"#)?;
+        db.execute(r"CREATE TABLE IF NOT EXISTS pastes (id TEXT, data BLOB);")?;
         Ok(Self { db })
     }
 
-    pub fn store_paste(&self, id: String, content: Bytes) {
+    pub fn store_paste(&self, id: &str, content: &Bytes) {
         self.db
             .prepare("INSERT INTO pastes (id, data) VALUES (?, ?);")
             .and_then(|mut stmt| {
-                stmt.bind((1, id.as_str()))?;
+                stmt.bind((1, id))?;
                 stmt.bind((2, content.iter().as_slice()))?;
                 stmt.next()
             })
@@ -52,4 +52,35 @@ pub fn generate_id() -> String {
             .map(char::from)
             .collect()
     })
+}
+
+#[cfg(test)]
+mod tests {
+    use actix_web::body::MessageBody;
+
+    use super::*;
+
+    #[test]
+    fn test_basic_paste() {
+        let store = PasteStore::new(":memory:").unwrap();
+        store.store_paste("hello", &b"world".try_into_bytes().unwrap());
+        let hello = store.get_paste("hello").unwrap();
+        assert_eq!("world".as_bytes(), hello.iter().as_slice());
+    }
+
+    #[test]
+    fn test_binary_paste() {
+        let store = PasteStore::new(":memory:").unwrap();
+        store.store_paste("hello", &[0x0, 0x1, 0x2, 0x3, 0xff].try_into_bytes().unwrap());
+        let hello = store.get_paste("hello").unwrap();
+        assert_eq!(&[0x0, 0x1, 0x2, 0x3, 0xff], hello.iter().as_slice());
+    }
+
+    #[test]
+    fn test_empty() {
+        let store = PasteStore::new(":memory:").unwrap();
+        store.store_paste("hello", &[].try_into_bytes().unwrap());
+        let hello = store.get_paste("hello").unwrap();
+        assert!(hello.is_empty());
+    }
 }
